53fc05b7e11c52d0840e0c7a7bfbe243aff0996f
siailya <sia.ilya.2017@gmail.com>
2023-09-30 14:28:12 +0300

    Подключение бэкенда

--- client/src/data/api/axiosInstance.ts
+++ client/src/data/api/axiosInstance.ts
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-  // url: 'https://kokoc.flint3s.ru/api/'
-  baseURL: 'https://run.mocky.io/v3/'
+  baseURL: 'https://kokoc.flint3s.ru/api/'
 })
